9145c47e06c78498fc05652b31f2aba7efece679
Kartik Singal <ka4tik@gmail.com>
2015-01-16 06:41:12 +0530

    added and configured parse.com api

---
+++ app/src/main/java/in/ka4tik/ribbit/RibbitApplication.java
@@ -0,0 +1,28 @@
+package in.ka4tik.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by ka4tik on 1/16/15.
+ */
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "f5iothZDTOV3s2HAB24AOgGub3nHeSSM2Bs6mMLj", "cRyYSYM0Xzk2xBlex3mfyGHhaaY4ZXHRgd9hINJN");
+
+//        ParseObject testObject = new ParseObject("TestObject");
+//        testObject.put("foo", "bar");
+//        testObject.saveInBackground();
+
+    }
+
+}
